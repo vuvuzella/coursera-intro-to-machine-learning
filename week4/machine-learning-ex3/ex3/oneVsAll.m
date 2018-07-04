d015f46e9a9968d10ad92f6b2f29e8a7d6dc2f9f
Jonathan II Tabac <c3267035@uon.edu.au>
2018-07-04 15:06:02 +1000

    Added oneVsAllPredict function to do predictions

--- week4/machine-learning-ex3/ex3/oneVsAll.m
+++ week4/machine-learning-ex3/ex3/oneVsAll.m
@@ -16,7 +16,6 @@ all_theta = zeros(num_labels, n + 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the following code to train num_labels
 %               logistic regression classifiers with regularization
@@ -58,7 +57,6 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
 for c = 1:num_labels
     bool_array_labels = (y == c);
     [theta] = fmincg(@(t)(lrCostFunction(t, X, bool_array_labels, lambda)), initial_theta, options);
-    
     all_theta(c, :) = transpose(theta);
 end
  
