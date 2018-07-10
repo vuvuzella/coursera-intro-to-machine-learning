7aeeee60c080ff80fc89f42eb619c8439a3d6fff
Jonathan II Tabac <c3267035@uon.edu.au>
2018-07-10 11:37:34 +1000

    Added sigmoid gradient function

--- week5/machine-learning-ex4/ex4/sigmoidGradient.m
+++ week5/machine-learning-ex4/ex4/sigmoidGradient.m
@@ -7,27 +7,10 @@ function g = sigmoidGradient(z)
 %   the gradient for each element.
 
 g = zeros(size(z));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+g = sigmoid(z) .* (1 - sigmoid(z));
 % =============================================================
-
-
-
 
 end
